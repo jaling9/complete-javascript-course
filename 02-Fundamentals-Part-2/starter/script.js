0b4e0a8eea7e0f1b979bd12537a0ba52516c8ec5
blurryFace9 <50992587+jaling9@users.noreply.github.com>
2023-01-16 23:17:26 +0800

    object.method

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -360,3 +360,41 @@ console.log(jonas);
 // "Jonas has 3 friends, and his best friend is called Michael"
 console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`);
 
+
+///////////////////////////////////////
+// Object Methods
+
+const jonas = {
+  firstName: 'Jonas',
+  lastName: 'Schmedtmann',
+  birthYeah: 1991,
+  job: 'teacher',
+  friends: ['Michael', 'Peter', 'Steven'],
+  hasDriversLicense: true,
+
+  // calcAge: function (birthYeah) {
+  //   return 2037 - birthYeah;
+  // }
+
+  // calcAge: function () {
+  //   // console.log(this);
+  //   return 2037 - this.birthYeah;
+  // }
+
+  calcAge: function () {
+    console.log(this.birthYeah);
+    // 因为是Jonas的方法,所以继承了jonas的属性,输出为1991
+    this.age = 2037 - this.birthYeah;
+    return this.age;
+  },
+
+  getSummary: function () {
+    return `${this.firstName} is a ${this.calcAge()}-year old 
+    ${jonas.job}, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license.`
+  }
+};
+
+  console.log(jonas.calcAge(1991)); 
+  // 因为是Jonas的方法,所以继承了jonas的属性,输出为1991
+  console.log(jonas.age)
+  console.log(jonas.getSummary());
